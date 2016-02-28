e489059afecb814b11e2249474578a26808e235c
teppeis <teppeis@gmail.com>
2016-02-28 17:24:49 +0900

    fix(index): support outDir option

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var fs = require('fs');
+var path = require('path');
 
 var espowerSource = require('espower-source');
 var minimatch = require('minimatch');
@@ -8,13 +9,14 @@ var ts = require('typescript');
 var TypeScriptSimple = require('typescript-simple').TypeScriptSimple;
 
 function espowerTypeScript(options) {
+  var cwd = options.cwd || process.cwd();
   var separator = (options.pattern.lastIndexOf('/', 0) === 0) ? '' : '/';
-  var pattern = options.cwd + separator + options.pattern;
-  var compilerOptions = convertCompilerOptions(options.compilerOptions, options.basepath);
+  var pattern = cwd + separator + options.pattern;
+  var compilerOptions = convertCompilerOptions(options.compilerOptions, options.basepath || cwd);
   var tss = new TypeScriptSimple(compilerOptions, false);
 
   function loadTypeScript(localModule, filepath) {
-    var result = tss.compile(fs.readFileSync(filepath, 'utf-8'));
+    var result = tss.compile(fs.readFileSync(filepath, 'utf-8'), path.relative(cwd, filepath));
     if (minimatch(filepath, pattern)) {
       result = espowerSource(result, filepath, options);
     }
@@ -30,7 +32,6 @@ function convertCompilerOptions(compilerOptions, basepath) {
     return null;
   }
 
-  var basepath = basepath || process.cwd();
   var converted = ts.convertCompilerOptionsFromJson(compilerOptions, basepath);
   if (converted.errors && converted.errors.length > 0) {
     var msg = converted.errors.map(function(e) {return e.messageText}).join(', ');
