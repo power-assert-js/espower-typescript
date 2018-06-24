720496ac84f834d74370c6bc8e0936330cb637d7
teppeis <teppeis@gmail.com>
2018-06-24 22:39:51 +0900

    feat: use ts-node

--- guess.js
+++ guess.js
@@ -1,8 +1,6 @@
 var fs = require('fs');
 var path = require('path');
 
-var ts = require('typescript');
-
 var pattern = 'test/**/*.@(ts|tsx)';
 var cwd = process.cwd();
 var packageData = require(path.join(cwd, 'package.json'));
@@ -14,30 +12,7 @@ if (packageData &&
   pattern = testDir + ((testDir.lastIndexOf('/', 0) === 0) ? '' : '/') + '**/*.@(ts|tsx)';
 }
 
-var tsconfigPath = ts.findConfigFile(cwd, fs.existsSync);
-var tsconfigBasepath = null;
-var compilerOptions = null;
-if (tsconfigPath) {
-  compilerOptions = parseTsConfig(tsconfigPath);
-  tsconfigBasepath = path.dirname(tsconfigPath);
-}
-
 require('./index')({
-    cwd: cwd,
-    pattern: pattern,
-    compilerOptions: compilerOptions,
-    basepath: tsconfigBasepath
+  cwd: cwd,
+  pattern: pattern,
 });
-
-function parseTsConfig(tsconfigPath) {
-  var parsed = ts.parseConfigFileTextToJson(tsconfigPath, fs.readFileSync(tsconfigPath, 'utf8'));
-  if (parsed.error) {
-    throw new Error(parsed.error.messageText);
-  }
-
-  if (!parsed.config || !parsed.config.compilerOptions) {
-    return null;
-  }
-
-  return parsed.config.compilerOptions;
-}
